f6d782fa6d7ebee56efda66baf1bf23678ce3a65
SabrinaChoque <sabrinaechoque@gmail.com>
2025-05-18 22:04:44 -0300

    Agrega componente Sabrina con estilos y navegación
    - Se creó el archivo Sabrina.jsx con datos personales y tecnologías usadas
    - Se añadió el archivo sabrina.css en carpeta styles
    - Se actualizó App.jsx para incluir la ruta /sabrina
    - Se actualizó Sidebar con el enlace a Sabrina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Sidebar from './components/Sidebar';
+import Sabrina from './pages/Sabrina';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <main style={{ flex: 1, padding: '1rem' }}>
           <Routes>
             <Route path="/" element={<Inicio />} />
+            <Route path="/sabrina" element={<Sabrina />} />
                     
 
           </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import '../styles/Sidebar.css';
 
+
 function Sidebar() {
   return (
     <nav className="sidebar">
     <h2>Grupo 7</h2>
       <ul>
         <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/sabrina">Sabrina</Link></li>
+
           </ul>
     </nav>
   );
